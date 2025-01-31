5eb9489913b6866571b2cb26c68bd98ef834f458
AnastasiiaUrvantseva <ana.urvantseva@gmail.com>
2025-01-31 11:25:29 -0300

    Initial commit

---
+++ Selenium_Facebook/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Selenium_Facebook
+{
+    internal class Program
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        static void Main(string[] args)
+        {
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            // Go to Facebook Page
+            driver.Manage().Cookies.DeleteAllCookies();
+            driver.Navigate().GoToUrl("https://www.facebook.com/?locale=en_US");
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            // Make browser full screen
+            driver.Manage().Window.Maximize();
+
+            IWebElement CreateNewAccount = driver.FindElement(By.CssSelector("a[data-testid = 'open-registration-form-button']"));
+            CreateNewAccount.Click();
+
+            IWebElement FirstName = driver.FindElement(By.Name("firstname"));
+            FirstName.SendKeys("Ana");
+
+            IWebElement LastName = driver.FindElement(By.Name("lastname"));
+            LastName.SendKeys("Wick");
+
+            IWebElement Email = driver.FindElement(By.Name("reg_email__"));
+            Email.SendKeys("anawick@gmail.com");
+
+            IWebElement Password = driver.FindElement(By.Name("reg_passwd__"));
+            Password.SendKeys("myawesomepassword");
+
+            IWebElement Month = driver.FindElement(By.Id("month"));
+            var selectElementMonth = new SelectElement(Month);
+            selectElementMonth.SelectByValue("12");
+
+            IWebElement Day = driver.FindElement(By.Id("day"));
+            var selectElementDay = new SelectElement(Day);
+            selectElementDay.SelectByValue("7");
+
+            IWebElement Year = driver.FindElement(By.Id("year"));
+            var selectElementYear = new SelectElement(Year);
+            selectElementYear.SelectByValue("2005");
+
+            IWebElement GenderButton = driver.FindElement(By.Id("sex"));
+            GenderButton.Click();
+
+            IWebElement SignUpButton = driver.FindElement(By.Name("websubmit"));
+            SignUpButton.Click();
+
+            Thread.Sleep(10000);
+
+            //For verigying an error message for fake account:
+            //IWebElement ErrorMessage = driver.FindElement(By.XPath("//*[@id=\"reg\"]/div/div"));
+            //Assert.That(ErrorMessage.Text.Contains("Sorry, we are not able to process your registration."));
+
+        }
+
+        [TearDown] 
+        public void CloseTest()
+        {
+            // Close the browser
+            driver.Quit();
+        }
+    }
+}
---
+++ Selenium_Facebook/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Selenium_Facebook")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Selenium_Facebook")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("aa0a24c7-4e09-4b38-8e67-30a9c4b1ec4f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
